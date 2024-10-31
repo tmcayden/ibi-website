cb5561e831ba696f99f6bb12154a11ee32e5040c
Cayden Hunsaker <caydenhunsaker@gmail.com>
2024-10-31 13:51:42 -0600

    project detail

--- ibi-vue/src/views/ProjectView.vue
+++ ibi-vue/src/views/ProjectView.vue
@@ -1,23 +1,32 @@
 <script setup>
 import { useToast } from 'primevue/usetoast'
 import { useUserStore } from '../stores/userStore'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { ref } from 'vue'
 import { supabase } from '../supabase'
 import { useProjectStore } from '../stores/projectStore'
 import Button from 'primevue/button'
+import BidRequest from '../components/BidRequest.vue'
+import Image from 'primevue/image'
+import { getFileUrl } from '../util/supabase/downloadFile'
+import { useScreenSizeStore } from '../stores/screenSizeStore'
 
 const props = defineProps({
   id: { type: [String, Number], required: true }
 })
 const toast = useToast()
 const user = useUserStore()
+const screenSize = useScreenSizeStore()
 const project = useProjectStore()
 
 const isLoading = ref(false)
 const images = ref([])
+const imagePaths = ref([])
 const uploading = ref(false)
-const uploadedFile = ref(null)
+
+const galleryWidth = computed(() => {
+  if (screenSize.isMd) return '350px'
+})
 
 async function refresh() {
   isLoading.value = true
@@ -28,6 +37,18 @@ async function refresh() {
 
   images.value = data
   isLoading.value = false
+  getImagePaths()
+}
+
+async function getImagePaths()
+{
+  var paths = []
+  for (const image of images.value)
+  {
+    const data = await getFileUrl('projects', image.image_path)
+    paths.push(data)
+  }
+  imagePaths.value = paths
 }
 
 async function uploadPhoto(event)
@@ -53,8 +74,14 @@ async function uploadPhoto(event)
     })
     if (error)
     {
-      toast.add({ severity: 'error', summary: 'Error linking image to project', detail: error.message, life: 2000 })
+      return toast.add({ severity: 'error', summary: 'Error linking image to project', detail: error.message, life: 2000 })
     }
+    toast.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Image uploaded successfully',
+      life: 2000
+    })
   }
   catch(e)
   {
@@ -69,8 +96,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-white p-3">
-    <Button as="label" for="newProjectImage" label="Add Image" icon="pi pi-upload" :disabled="uploading"/>
-    <input style="visibility: hidden;" type="file" id="newProjectImage" accept="image/*" @change="uploadPhoto" :disabled="uploading"></input>
+  <div class="p-3" :style="{
+    backgroundImage: `url('/bw-bg-2.jpg')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '100%'
+  }">
+    <div class="flex flex-col lg:flex-row justify-between items-center 2xl:p-24 2xl:pt-8 2xl:pb-8 pt-8 pb-8">
+      <div class="lg:w-1/2 tracking-widest leading-8 indent">{{ project.project.description }}</div>
+      <BidRequest class='mt-8 w-full md:w-96' />
+    </div>
+    <Button v-if=user.isLoggedIn as="label" for="newProjectImage" label="Add Image" icon="pi pi-upload" :disabled="uploading"/>
+    <input v-if=user.isLoggedIn style="visibility: hidden;" type="file" id="newProjectImage" accept="image/*" @change="uploadPhoto" :disabled="uploading" />
+  </div>
+  <div class="flex flex-wrap justify-center">
+    <div v-for="image in imagePaths" :key="image.id" class="m-2 flex flex-wrap">
+      <Image :src="image" alt="Project Image" :width="galleryWidth" class='' preview />
+    </div>
   </div>
 </template>
